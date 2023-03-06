f0fe4d38faf99cc262b590c1ffdb34cb9f8d1557
Savan Patel <savan.patel@pagarbook.com>
2023-03-06 10:02:10 +0530

    add antd

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ProductCard from "./product/product-card";
+import { Button, Modal } from "antd";
+// import { Box, Button, Modal, Typography } from "@mui/material";
 
 const Home = () => {
   const [productsList, setProductsList] = useState([
@@ -68,11 +70,73 @@ const Home = () => {
     },
   ]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      {productsList.map((product) => (
-        <ProductCard product={product} />
-      ))}
+    <div>
+      {/* <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {productsList.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div> */}
+      {/* 
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal open={isModalOpen} onClose={handleClose}>
+        <Box
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal> */}
+
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </div>
   );
 };
